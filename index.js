ab87f2ac1bcfd850d8b9eba18e59a5e1f2ca77db
sumanga wimaladasa <sumanga0000@gmail.com>
2023-11-13 19:06:20 +0530

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const ws = require('ws')
 
 const app = express()
-const httpServer = app.listen(process.env.PORT)
+const httpServer = app.listen(process.env.PORT,()=>console.log('App is running'))
 
 const wsServer = new ws.Server({ noServer: true })
 
@@ -10,4 +10,8 @@ httpServer.on('upgrade', (req, socket, head) => {
   wsServer.handleUpgrade(req, socket, head, (ws) => {
     wsServer.emit('connection', ws, req)
   })
+})
+app.get('/',(req,res)=>{
+
+    res.end('hello')
 })
